93f8cfd272253832ad5fadf6c202400761abcc3c
deadly-unicorn <linatic@pm.me>
2023-01-25 05:03:37 +0200

    added UI while the rest is loading. it shows Error: window is not defined tho?

---
+++ app/dataTest/otherTest/testing/loading.tsx
@@ -0,0 +1,62 @@
+
+export default function Screen(){
+  return(
+    <>
+
+      <div 
+        className="h-screen flex flex-col justify-center items-center">
+      
+      
+        <div>
+
+          <p>Add values to dataEntered:</p>
+          
+        </div>
+
+
+        <div>
+
+          <input 
+            value="" //prevent users from typing while loading
+            className="rounded-md "/>
+        
+        </div>
+
+
+        <div>
+
+
+          <button 
+            className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
+          >
+            
+            Submit
+
+          </button>
+
+          <button
+            className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
+          >
+              
+            Erase ALL Data
+          
+          </button>
+
+
+        </div>
+
+
+        <div className="h-2/5 w-11/12 overflow-auto">
+          <div className="h-4 m-2 text-center">
+
+            <p>Loading Info...</p>
+        
+          </div>
+        </div>
+
+
+      </div>
+
+
+    </>
+  )}
