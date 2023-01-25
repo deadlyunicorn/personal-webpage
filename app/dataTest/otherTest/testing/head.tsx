f211ce8b3787be27cd237b03c27da3544178e96f
deadly-unicorn <linatic@pm.me>
2023-01-25 05:04:29 +0200

    changed the tittle and the favicon

---
+++ app/dataTest/otherTest/testing/head.tsx
@@ -0,0 +1,12 @@
+export default function Head(){
+  
+  return (
+    <>
+      <title>Database App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="This app edits the data on my Realtime Database, by Firebase." />
+      <link rel="icon" href="/database.png" />
+    </>
+  )
+  
+}
