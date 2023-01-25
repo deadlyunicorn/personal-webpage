e6fbde39c43416a894fb33cbc1fee8fd1dddf0dd
deadly-unicorn <linatic@pm.me>
2023-01-25 03:10:12 +0200

    detailed info component on a SSR file

---
+++ app/dataTest/otherTest/testing/detailedInfo.tsx
@@ -0,0 +1,39 @@
+//server side rendering for getInfo
+
+
+/// https://bobbyhadz.com/blog/react-typescript-pass-object-as-props
+interface dataTypesInterface{
+  dataEntered:string[];
+  fetchedDataString:string
+}
+
+
+export default function DetailedInfo(props:dataTypesInterface){
+  return( ///just to avoid the Warning: possibly null
+      <>
+      <div className="grid grid-cols-2 gap-4 ">
+        <div className="border-l-8 border-t-4 border-black rounded-r-lg rounded-b-lg bg-white bg-opacity-50 shadow-lg h-auto overflow-auto">
+          <p>
+            Last database insertion was:<br/> &apos;{(props.dataEntered[props.dataEntered.length-1])}&apos;
+          </p>
+        </div>
+        <div className="border-l-8 border-t-4 border-black rounded-r-lg rounded-b-lg bg-white bg-opacity-50 shadow-lg h-auto overflow-auto">
+         <p>
+            The dataEntered property stores:<br/>{props.dataEntered.length} value(s)
+          </p>
+        </div>
+        <div className="border-l-8 border-t-4 border-black rounded-r-lg rounded-b-lg bg-white bg-opacity-50 shadow-lg h-auto overflow-auto ">
+          <p>
+            All database entries:<br/> {(props.fetchedDataString)}
+          </p>
+        </div>
+        <div className="border-l-8 border-t-4 border-black rounded-r-lg rounded-b-lg bg-white bg-opacity-50 shadow-lg h-auto overflow-auto">
+          <p>
+            dataEntered entries:<br/> {JSON.stringify(props.dataEntered)}
+          </p>
+        </div>
+
+      </div>
+      </>
+    )
+}
