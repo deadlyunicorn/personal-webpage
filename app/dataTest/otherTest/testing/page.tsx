155bdc6f1169d3277657fe31dff79d85410bc393
deadly-unicorn <linatic@pm.me>
2023-01-26 02:54:20 +0200

    Now not uploading empty strings

--- app/dataTest/otherTest/testing/page.tsx
+++ app/dataTest/otherTest/testing/page.tsx
@@ -1,43 +1,46 @@
 'use client'
 
-//Imported for Navigation
-import Link from "next/link"
+//imports
 
-//useState,useRef
-import { useState,useRef } from "react"
+      //Imported for Navigation
+      import Link from "next/link"
+      
+      //useState,useRef
+      import { useState,useRef } from "react"
+      
+      
+      
+      ///rewriting page.tsx(old) from scratch because
+      ///I couldn't correct it by changing parts..
+      
+      ///importing firebase libraries
+      
+      //getting the database config
+      import {database} from "../../../firebaseConfig"
+      /*
+      database = getDatabase(app)
+      app = initializeApp(firebaseConfig);
 
+      */
+    
+    
+      ///needed to select routes in our database
+      import {ref,set, onValue } from "firebase/database"
+      
+      //needed to render live database updates
+      import { useEffect } from "react"
+      
+      
+      
+      //Import component for detailed Info
+      import DetailedInfo from "./detailedInfo"
 
-
-///rewriting page.tsx(old) from scratch because
-///I couldn't correct it by changing parts..
-
-///importing firebase libraries
-
-//getting the database config
-import {database} from "../../../firebaseConfig"
-/*
-database = getDatabase(app)
-app = initializeApp(firebaseConfig);
-
-*/
-
-
-///needed to select routes in our database
-import {ref,set, onValue } from "firebase/database"
-
-//needed to render live database updates
-import { useEffect } from "react"
-
-
-
-//Import component for detailed Info
-import DetailedInfo from "./detailedInfo"
+//
 
 ///App Function
 export default function DataBaseFetch(){
 
-  ///updating userInput with react useState
-  const [userInput,setUserInput]=useState("")
+
   
 
   ////////
@@ -50,10 +53,12 @@ export default function DataBaseFetch(){
 
   ///Rendering live changes
 
+  
   const [hasLoaded,setHasLoaded]=useState(false);
 
   //needed for storing fetched data//
   type dataType= string[] | null
+
   interface dataTypesInterface{
     dataEntered:string[] | null 
   }
@@ -69,7 +74,6 @@ export default function DataBaseFetch(){
     dataEntered:null
   }; //the initial value of the fetchedData (no data, null for all values) // check useEffect if (data==null)
   
-
   
   //on-off switch that tells if there is data or not
 
@@ -84,12 +88,15 @@ export default function DataBaseFetch(){
        //that there is data
 
 
-  console.time('fetching data...');
   
   useEffect(()=>{
     //
+    const dataReference = ref(database,'users/'); //added because of
+    const fetchedNull={ //React Hook useEffect has missing dependencies: 'dataReference' and 'fetchedNull'.
+      dataEntered:null      // Either include them or remove the dependency array
+    };
 
-    return onValue (dataReference,  async (snapshot) => { // return the unsubscriber https://stackoverflow.com/questions/71008942/mix-useeffect-and-firebases-onvalue 
+    return (onValue (dataReference,  (snapshot) => { // return the unsubscriber https://stackoverflow.com/questions/71008942/mix-useeffect-and-firebases-onvalue 
       const data =  snapshot.val() //value of updated database.
  
       if (data != null) {
@@ -98,21 +105,22 @@ export default function DataBaseFetch(){
  
        }
       else {
-        setFetchedData(data)
+        setFetchedData(fetchedNull) 
           //At first I thought this was not needed,
           //but as the app constantly listens for new changes
           //if we delete the data, then the app continues thinking
           //that there is data
         setHasLoaded(true)
       }
-    })
+      console.log("reload..")
+    }))
  
 
     //
     
 
-  })
-  console.timeEnd('fetched data...');
+  },[hasLoaded])//hasLoaded is not needed here I believe, but it stops infinite page rerenders. 
+  //will remove when I find another workaround
 
 
   ///writing data - this works similarly to a react setState()
@@ -145,11 +153,10 @@ export default function DataBaseFetch(){
 
   ///testing code
   ///added useRef() to the input "onChange", that way I think we get less re-renders
-  const inputRef = useRef('abc');
+  const inputRef = useRef(); //default value=null
   const handleInputChange = (event:any) =>{
     inputRef.current = (event.target.value)
   }
-  const [currValue,setCurrValue]=useState('')
 
 
 
@@ -164,96 +171,119 @@ export default function DataBaseFetch(){
   ///Main App JSX
   return(
     <>
-    <div id="navBar" 
-      className="bg-opacity-0 absolute">
-      <Link href='./..'  target="_parent"><h3>Go back...</h3></Link>
-    </div>
+      <div id="navBar" 
+        className="bg-opacity-0 absolute">
+        <Link href='./..'  target="_parent"><h3>Go back...</h3></Link>
+      </div>
 
 
-    <div id="page_div"
-    className="h-screen flex flex-col justify-center items-center">
-      
-      
-      <div id="instruction_div">
-        <p>Add values to dataEntered:</p>
+      <div id="page_div"
+      className="h-screen flex flex-col justify-center items-center">
         
-      </div>
-
-
-      <div id="input_div">
-
-        <input 
-          className="rounded-md "
-          onChange={(e)=>{handleInputChange(e)}
-            //needed so that we store the userInput 
-            //in some variable.
-            //useRef() for less re-renders.
-          }/>
-      
-      </div>
+        <div id="instruction_div">
+          <p>Add values to dataEntered:</p>
           
-      <div id="button_div">
+        </div>
 
 
-        <button 
-          className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
-          
+        <div id="input_div">
 
-            onClick={()=>{
-              if (!(fetchedData.dataEntered!=null)){
-
-                writeData([inputRef.current]) /**
-                //I initially thought that "here we can put either userInput or submitInput" 
-                //BUT I was wrong
-                //as I realized that this way it doesn't get updated on the database
-              //unless you pressed the button twice
-              //this might be due to some delay that accompanies setting SubmitInput
-              //while reading its data probably is almost instant. **/
-            }
-            else{ ///check how i am going to fix this :)
-              addData(inputRef.current)
-            }
-          }}>
-          
-          Submit
-
-        </button>
-
-        <button
-          className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
-          
-          onClick={()=>{
-            writeData(null)
-          }}>
+          <input 
+            className="rounded-md "
+            onChange={(e)=>{handleInputChange(e)}}
+              //needed so that we store the userInput 
+              //in some variable.
+              //useRef() for less re-renders.
+          />
+        
+        </div>
             
-          Erase ALL Data
-        
-        </button>
+        <div id="button_div">
+
+
+          <button 
+            className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
+            
+
+              onClick={()=>{
+
+
+                if ((fetchedData.dataEntered==null)){
+  //used for debugging:D                  console.log("1")
+                  if(inputRef.current!=null){
+
+                    
+                    if(([inputRef.current].toString()).replace(/\s/g,"").length!==0){
+                      
+                      writeData([inputRef.current])
+                      
+                    }
+                  } /**
+                  //I initially thought that "here we can put either userInput or submitInput" 
+                  //BUT I was wrong
+                  //as I realized that this way it doesn't get updated on the database
+                //unless you pressed the button twice
+                //this might be due to some delay that accompanies setting SubmitInput
+                //while reading its data probably is almost instant. **/
+              }
+              else{ ///check how i am going to fix this :)
+                
+                if(inputRef.current!=null){
+
+
+                  if(([inputRef.current].toString()).replace(/\s/g,"").length!==0){ //check if input is blank (only spaces)
+                     
+                    //i tried using useState, but Submit wasn't working on first click
+                    // (it need more). Also I am tired so I wont try to find a way to
+                    // store all of this expression in a variable or smth
+                    
+                    addData(inputRef.current)
+                  }
+
+                }
+              }
+              }}>
+            
+            Submit
+
+          </button>
+
+          <button
+            className="mt-2 bg-slate-200 rounded-md p-1 border-2 border-l-4 border-l-teal-200 border-t-teal-200 shadow-lg"
+            
+            onClick={()=>{
+              writeData(null)
+            }}>
+              
+            Erase ALL Data
+          
+          </button>
+
+
+        </div>
+
+
+        <div id="info_div"
+          className="h-96 w-11/12 m-2 text-center max-w-xl overflow-auto ">
+            {
+            (!hasLoaded)&&<p>Loading Info...</p>
+            }
+
+            {
+              (hasLoaded&&(fetchedData.dataEntered==null))&&<p>There is currently no data...</p>
+            }
+
+            {
+              //if we add more properties to fetchedData
+              //we need to think about dataEntered being null
+              (hasLoaded&&(fetchedData.dataEntered!=null)) && <DetailedInfo dataEntered={fetchedData.dataEntered} fetchedDataString={JSON.stringify(fetchedData)}/>
+            }
+
+        </div>
+
 
 
       </div>
-
-
-      <div id="info_div"
-        className="h-96 w-11/12 m-2 text-center max-w-xl overflow-auto ">
-          {
-          (!hasLoaded)&&<p>Loading Info...</p>
-          }
-
-          {
-            //if we add more properties to fetchedData
-            //we need to think about dataEntered being null
-            (fetchedData.dataEntered!=null) && <DetailedInfo dataEntered={fetchedData.dataEntered} fetchedDataString={JSON.stringify(fetchedData)}/>
-          }
-
-          {
-            ((fetchedData.dataEntered==null)&&hasLoaded)&&<p>There is currently no data...</p>
-          }
-      </div>
-
-
-
-    </div>
     </>
   )
 
