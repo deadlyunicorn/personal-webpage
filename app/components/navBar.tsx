625d3a95d1c65111b622481e5cf819dada4805fc
deadly-unicorn <linatic@pm.me>
2023-01-29 00:37:04 +0200

    some css adjustments for a more responsive layout I think Chrome's tool is trolling when you emulate some device screens..

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,13 +1,13 @@
 import Link from "next/link"
 export default function NavBar(){
 
-  return(<nav className='absolute bg-neutral-900 w-screen rounded-b-sm shadow-md  '>
-<div className='p-2 flex justify-around '>
-  <div className='border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/"}><button>Homepage 🏠</button></Link> </div>
+  return(<nav className='absolute bg-neutral-900 w-screen rounded-b-sm shadow-md sm:h-11 h-16 max-h-screen '>
+<div className='p-2 flex items-center justify-around h-full'>
+  <div className=' sm:border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/"}><button>Homepage <span className="hidden sm:inline">🏠</span></button></Link> </div>
   <div className='border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/dataTest"}><button>DataTest</button></Link> </div>
-  <div className='border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/dataTest/otherTest"}><button>OtherTest</button></Link> </div>
+  <div className='sm:border-x border-r border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/dataTest/otherTest"}><button>OtherTest</button></Link> </div>
   <div className='group'>
-      <div className='border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/"}><button>Testing 🔥</button></Link> </div>
+      <div className=' sm:border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 '><Link href={"/"}><button>Testing <span className="hidden sm:inline"> 🔥</span></button></Link> </div>
         {/* 2 divs above we use group and also below we set default to invisible, not hidden */}
       <div className='group-hover:visible group-focus-within:visible  hover:visible invisible absolute bg-neutral-900  rounded-md pt-2 w-auto overflow-hidden'>
         <div className='border-x border-slate-700 hover:border-slate-500 px-2 sm:text-[2vh] font-light font-sans text-cyan-300  focus-within:text-red-200 hover:text-cyan-200 border-t py-1 '><Link href={"/dataTest/otherTest/testing"}><button className='w-full text-left'>Testing1<span className='text-[1vh]'>🔥🔥</span></button></Link> </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,84 +3,94 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gradient-to-b from-blue-300 to-red-300 overflow-hidden">
-      <div className=" h-screen flex flex-col justify-center items-center ">
-        <div className="text-white drop-shadow-[1px_1px_3px_rgba(200,50,200,1)] sm:text-[3vh] md:text-[5vh] md:pb-3 text-[2vh] hover:animate-pulse animate-bounce">DeadlyUnicorn</div>
-        <div className=" max-w-md w-3/6 shadow-xl rounded-t-md sm:h-72 overflow-hidden "><a href="https://www.linkedin.com/in/Alexandros-Petrache/" target="_blank" rel="noopener noreferrer"><Image src="/profilePicture_withShadow.png" width="500" height="500" alt="My online avatar, a purple unicorn." className="sm:-translate-y-12  "/></a></div>
-        <div 
-          className="border-white border-t border-l max-w-md w-3/6 text-center bg-red-100 bg-opacity-50 rounded-b-md py-2 shadow-lg">
-              <div className="font-light p-1">
+    <div className="h-screen bg-gradient-to-b from-blue-300 to-red-300 overflow-hidden flex flex-col justify-center items-center  max-h-screen ">
+      <div className="  h-auto sm:mt-0  sm:max-w-xs lg:max-w-md max-w-xs mt-4 lg:h-auto  ">
+        <div className="  h-full  flex flex-col justify-center items-center overflow-hidden max-h-[80vh] ">
 
-              Just testing some stuff here. Feel&nbsp;free to explore this project, ask me any questions and&nbsp;
-                <a 
-                  href="https://www.linkedin.com/in/Alexandros-Petrache/" 
-                  target="_blank" rel="noopener noreferrer" 
-                  className=" text-blue-600 hover:text-blue-400 focus-within:text-rose-600 focus:"><button className="hover:underline ">
-                    connect
-                </button></a> 
-                &nbsp;with&nbsp;me.
-              </div>
-              <div className="border-t border-black m-2">
-                <p className="p-1 font-semibold "> BIO</p>  
-                <p className="font-light">I am a Web developer.</p>
-                <p className="font-light">Currently I am interested&nbsp;in:</p>
-                <ol className="list-disc list-inside text-left mt-2">
-                  <li>
-                    <a 
-                      href="https://nextjs.org"
-                      target="_blank" rel="noopener noreferrer">
-                        
-                      <button>
-                        <span 
-                          className="p-1 drop-shadow-[-1px_1.3px_1.5px_rgba(0,0,0,1)]  text-slate-200 gradient font-bold text-lg">
-                            NextJS
-                        </span>
-                      </button>
-                    </a>
-                  </li>
+          <div className="text-white drop-shadow-[1px_1px_3px_rgba(200,50,200,1)] sm:text-[4vh] text-[3vh] hover:animate-pulse animate-bounce  ">DeadlyUnicorn</div>
+          
+          <div className="shadow-xl rounded-t-md sm:h-56  h-48 overflow-hidden border border-white w-full lg:h-96 "><a href="https://www.linkedin.com/in/Alexandros-Petrache/" target="_blank" rel="noopener noreferrer">
+            <Image src="/profilePicture_withShadow.png" width="500" height="500" alt="My online avatar, a purple unicorn." className="md:-translate-y-14  -translate-y-12 "/></a>
+          </div>
+          
+          <div 
+            className="border-white border-t border-l  text-center bg-red-100 bg-opacity-50 rounded-b-md shadow-lg ">
 
-                  <li>
-                    <a 
-                      href="https://svelte.dev"
-                      target="_blank" rel="noopener noreferrer">
-                      <button>
-                        <span 
-                        className="p-1 drop-shadow-[-1px_1.3px_2px_rgba(255,60,60,1)]  text-white font-bold text-lg">Svelte
-                        </span>
-                      </button>
-                    </a>
-                  </li>
+                <div className="font-light p-1">
 
-                  <li>
-                    <a 
-                      href="https://tailwindcss.com"
-                      target="_blank" rel="noopener noreferrer">
-                      <button>
-                        <span 
-                        className="p-1 drop-shadow-[-1px_1.3px_1.6px_rgba(40,90,155,1)]  text-white font-bold text-lg">TailwindCSS
-                        </span>
-                      </button>
-                    </a>
-                  </li>
-                  <li>
-                    <a 
-                      href="https://reactnative.dev"
-                      target="_blank" rel="noopener noreferrer">
-                      <button>
-                        <span 
-                        className="drop-shadow-[0px_-0.3px_1.4px_rgba(0,0,0,1)]">
+                Just testing some stuff here. Feel&nbsp;free to explore this project, ask me any questions and&nbsp;
+                  <a 
+                    href="https://www.linkedin.com/in/Alexandros-Petrache/" 
+                    target="_blank" rel="noopener noreferrer" 
+                    className=" text-blue-600 hover:text-blue-400 focus-within:text-rose-600 focus:"><button className="hover:underline ">
+                      connect
+                  </button></a> 
+                  &nbsp;with&nbsp;me.
+                </div>
+                <div className="border-t border-black m-2">
+                  <p className="p-1 font-semibold "> BIO</p>  
+                  <p className="font-light">I am a Web developer.</p>
+                  <p className="font-light">Currently I am interested&nbsp;in:</p>
+                  <ol className="list-disc list-inside text-left mt-2">
+                    <li>
+                      <a 
+                        href="https://nextjs.org"
+                        target="_blank" rel="noopener noreferrer">
+                          
+                        <button>
                           <span 
-                            className="p-1 drop-shadow-[-0.9px_-0.6px_0.4px_rgba(255,255,255,1)]  text-blue-300 hover:text-cyan-300 font-semibold text-lg ">
-                              ReactNative
+                            className="p-1 drop-shadow-[-1px_1.3px_1.5px_rgba(0,0,0,1)]  text-slate-200 gradient font-bold text-lg">
+                              NextJS
                           </span>
-                        </span>
-                      </button>
-                    </a>
-                  </li>
-                </ol>
-              </div>
-                  
+                        </button>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a 
+                        href="https://svelte.dev"
+                        target="_blank" rel="noopener noreferrer">
+                        <button>
+                          <span 
+                          className="p-1 drop-shadow-[-1px_1.3px_2px_rgba(255,60,60,1)]  text-white font-bold text-lg">Svelte
+                          </span>
+                        </button>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a 
+                        href="https://tailwindcss.com"
+                        target="_blank" rel="noopener noreferrer">
+                        <button>
+                          <span 
+                          className="p-1 drop-shadow-[-1px_1.3px_1.6px_rgba(40,90,155,1)]  text-white font-bold text-lg">TailwindCSS
+                          </span>
+                        </button>
+                      </a>
+                    </li>
+                    <li>
+                      <a 
+                        href="https://reactnative.dev"
+                        target="_blank" rel="noopener noreferrer">
+                        <button>
+                          <span 
+                          className="drop-shadow-[0px_-0.3px_1.4px_rgba(0,0,0,1)]">
+                            <span 
+                              className="p-1 drop-shadow-[-0.9px_-0.6px_0.4px_rgba(255,255,255,1)]  text-blue-300 hover:text-cyan-300 font-semibold text-lg ">
+                                ReactNative
+                            </span>
+                          </span>
+                        </button>
+                      </a>
+                    </li>
+                  </ol>
+                </div>
+                    
+          </div>
+
         </div>
+
       
       </div>
     </div>
