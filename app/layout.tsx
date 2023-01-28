7c12a7d59286369ece7dc7a1e065b18ea804f284
deadly-unicorn <linatic@pm.me>
2023-01-28 03:07:38 +0200

    added default (global?) loading screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { AnalyticsWrapper } from './components/analytics';
-
+import NavBar from './components/navBar';
 export default function RootLayout({
   children,
 }: {
@@ -13,7 +13,9 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
+   
       <body>
+        <NavBar/>
         {children}
         <AnalyticsWrapper/>  
       </body>
