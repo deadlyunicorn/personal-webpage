c9e92b7d6807e578526026615edc8c1e069d2b42
deadly-unicorn <linatic@pm.me>
2023-01-29 00:37:20 +0200

    chatApp page

---
+++ app/chatApp/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function ChatStartPage(){
+
+    return(
+      <>
+        <div className="h-screen -z-10">
+          <div className="sm:pt-11 h-full pt-16">
+            <div className="flex flex-col justify-center items-center h-full border-red-300 border">
+              <div>
+                hello world
+              </div>
+            </div>
+
+          </div>
+        </div>
+      
+      </>
+    )
+}
