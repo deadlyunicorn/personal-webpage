6c6e8207232b13b72971d28efb9d7276ddb6ccbe
deadly-unicorn <linatic@pm.me>
2023-01-29 04:58:44 +0200

    Great CSS fix. Works great on every device

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -2,12 +2,13 @@ import Image from "next/image"
 export default function Screen(){
   return(
     <>
-      <div className="h-screen absolute w-screen bg-gradient-to-b from-neutral-800 to-neutral-700 z-10 flex flex-col justify-center items-center ">
-        <div className="flex flex-col  items-center justify-center   p-2  ">
+      <div className="h-screen absolute w-screen bg-gradient-to-b from-neutral-800 to-neutral-700 z-20 overflow-auto flex-col  items-center justify-center flex">
+      
+        <div className="flex flex-col  items-center justify-center   p-20 ">
 
-          <Image alt=""  src="/loading_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden   -left-2" />
+         <Image alt=""  src="/loading_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48  " />
           <div className="text-blue-200 animate-pulse text-3xl"> Loading.. </div>
-          <Image alt=""  src="/loading_centered_single_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden " />
+          <Image alt=""  src="/loading_centered_single_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 " />
           
         </div>
 
