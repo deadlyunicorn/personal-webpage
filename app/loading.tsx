9f2a566a5b83b3668158ab5e2aae89cbc879f508
deadly-unicorn <linatic@pm.me>
2023-01-29 02:25:38 +0200

    css fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,11 +3,11 @@ export default function Screen(){
   return(
     <>
       <div className="h-screen absolute w-screen bg-gradient-to-b from-neutral-800 to-neutral-700 z-10 flex flex-col justify-center items-center ">
-        <div className="flex flex-row hover:flex-col  items-center justify-center   w-screen p-2">
+        <div className="flex flex-col  items-center justify-center   p-2  ">
 
-          <Image alt=""  src="/loading_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden" />
+          <Image alt=""  src="/loading_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden   -left-2" />
           <div className="text-blue-200 animate-pulse text-3xl"> Loading.. </div>
-          <Image alt=""  src="/loading_centered_single_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden" />
+          <Image alt=""  src="/loading_centered_single_centered.svg" width="64" height="64" className="animate-spin  w-48 h-48 overflow-hidden " />
           
         </div>
 
