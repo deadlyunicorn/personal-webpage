39e31095a06030116a94b8f8773aab2b5b76028b
deadly-unicorn <retroalex1008@gmail.com>
2023-05-05 23:00:36 +0300

    css effect on invisible button now selecting it looks like you are selecting the div box

--- app/(components)/navBar.tsx
+++ app/(components)/navBar.tsx
@@ -8,8 +8,8 @@ export default function NavigationBar(){
     id="menu"
     className="
       fixed top-10 sm:left-10 left-4
-      max-w-[20px] max-h-5 overflow-hidden
-      text-xl p-4
+      max-w-[40px] max-h-10 overflow-hidden
+      text-xl
       
       border-white border-2 bg-white rounded-md
       
@@ -17,6 +17,7 @@ export default function NavigationBar(){
       sm:hover:text-white hover:text-cyan-300 text-white
       sm:focus:text-white focus:text-cyan-300
       duration-500
+
       
       sm:hover:bg-opacity-0 
       sm:focus:bg-opacity-0 
@@ -24,25 +25,30 @@ export default function NavigationBar(){
       hover:max-h-40 hover:max-w-[160px]
       focus:max-h-40 focus:max-w-[160px]
       focus-within:max-h-40 focus-within:max-w-[160px]
-
-
-
-
-      flex flex-col justify-evenly z-10
       ">
-          <button>  </button>
-          <Link href="/" tabIndex={0}>Homepage</Link>
-          <Link href="/info" tabIndex={0}>Information</Link>
-          <LinkLine href="https://mongodb-social-app.vercel.app/">
-            <span className="text-red-100 hover:text-inherit underline">
-              Latest
-            </span>
-          </LinkLine>
-          <LinkLine href="https://mongodb-social-app.vercel.app/">
-            <span className="text-red-100 hover:text-inherit underline">
-              {/* Projects */}
-            </span>
-          </LinkLine>
+          <button className="absolute -z-10 w-full h-full">
+          </button>
+
+          <div 
+            className="
+              flex flex-col justify-evenly z-10 p-4">
+
+            <Link href="/" tabIndex={0}>Homepage</Link>
+            <Link href="/info" tabIndex={0}>Information</Link>
+            <LinkLine href="https://mongodb-social-app.vercel.app/">
+              <span className="text-red-100 hover:text-inherit underline">
+                {/* focus text doesn't get cyan.. Actually fixed, you added the class to the span... */}
+                Latest
+              </span>
+            </LinkLine>
+            <LinkLine href="https://mongodb-social-app.vercel.app/">
+              <span className="text-red-100 hover:text-inherit underline">
+                {/* Projects */}
+              </span>
+            </LinkLine>
+
+          </div>
+
         
 
     </div>
